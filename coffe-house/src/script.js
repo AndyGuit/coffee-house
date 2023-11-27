518f6db07d9487a7747fcd14609f47d66d06b7fc
AndyGuit <totalslash@msn.com>
2023-11-27 21:15:00 +0100

    feat: add burger menu

--- coffe-house/src/script.js
+++ coffe-house/src/script.js
@@ -1,1 +1,12 @@
-console.log('yes')
+/* ========= Burger Menu =========== */
+const burgerButton = document.querySelector('.header__burger-button');
+const navPage = document.querySelector('.header__nav--page');
+const navMenu = document.querySelector('.header__nav--menu');
+
+function toggleBurgerMenu() {
+  burgerButton.classList.toggle('active');
+  navPage.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+burgerButton.addEventListener('click', toggleBurgerMenu)
